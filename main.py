88b2ca8825044b08ad6c9d210eb516f9b402e79b
Greg W <1514406+fatbotgw@users.noreply.github.com>
2025-03-05 21:59:38 -0500

    Added groups, refactored to use groups

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")  # noqa: F405
     print(f"Screen height: {SCREEN_HEIGHT}")  # noqa: F405
     pygame.init()
-    
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # noqa: F405
 
     game_clock = pygame.time.Clock()
     dt = 0
 
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+    x = SCREEN_WIDTH / 2  # noqa: F405
+    y = SCREEN_HEIGHT / 2  # noqa: F405
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(x, y)
 
     while True:
@@ -24,15 +29,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(color='black')
-        player.update(dt)
-        player.draw(screen)
+        screen.fill(color="black")
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
-
-        
-
-
 
 
 if __name__ == "__main__":
